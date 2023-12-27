3bb77874869cf330503754aaf8eb6978c0410457
hoaithu842 <hoaithu842@gmail.com>
2023-12-28 00:32:07 +0700

    update form report
    except captcha & wysiwyg

--- controllers/danController.js
+++ controllers/danController.js
@@ -8,15 +8,6 @@ controller.dsBaoCao = (req, res) => {
 	res.render('dan_dsbaocao', { title: "Báo cáo đã gửi" , dsbaocao: true});
 }
 
-controller.formBaoCao = (req, res) => {
-	res.render("dan_guibaocao")
-}
-
-controller.testtest = (req, res) => {
-	console.log(req.body);
-	res.render("dan_guibaocao")
-}
-
 controller.dangnhap = (req, res) => {
 	res.render('dangnhap', { title: "Đăng nhập" , dangnhap: true});
 }
--- routes/danRouter.js
+++ routes/danRouter.js
@@ -4,9 +4,6 @@ const router = express.Router();
 
 router.get("/", controller.show);
 router.get("/dsBaoCao", controller.dsBaoCao);
-router.get("/guiBaoCao", controller.formBaoCao);
 router.get("/dangnhap", controller.dangnhap);
-
-router.post("/testtest", controller.testtest);
 
 module.exports = router;
