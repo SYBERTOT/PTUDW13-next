47d09d450b9b09bc0be31e046d2402e74231ced4
hoaithu842 <hoaithu842@gmail.com>
2023-12-29 18:30:49 +0700

    captcha & get data submitted

--- controllers/danController.js
+++ controllers/danController.js
@@ -2,14 +2,27 @@ const controller = {};
 
 controller.show = (req, res) => {
 	res.render('index', { title: "Trang chủ" , trangchu: true});
-}
+};
 
 controller.dsBaoCao = (req, res) => {
 	res.render('dan_dsbaocao', { title: "Báo cáo đã gửi" , dsbaocao: true});
-}
+};
 
 controller.dangnhap = (req, res) => {
 	res.render('dangnhap', { title: "Đăng nhập" , dangnhap: true});
-}
+};
+
+controller.guiBaoCao = async (req, res) => {
+	console.log(req.body);
+	let { hinhthuc, hoten, email, sdt, noidung} = req.body;
+	// try {
+	// 	await models.?.create({
+	// 	});
+	// 	res.redirect('/'); //thay vi render lai
+	// } catch (error) {
+	// 	res.send("Không thể tạo báo cáo!");
+	// 	console.error(error);
+	// }
+};
 
 module.exports = controller;
--- routes/danRouter.js
+++ routes/danRouter.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const controller = require("../controllers/danController");
 const router = express.Router();
+const axios = require("axios");
+const YOUR_RECAPTCHA_SECRET_KEY = '6Le4ZC8pAAAAAGJ2UkMgQm8AyeMrHrwmJYnwxtLg';
 
 router.get("/", controller.show);
 router.get("/dsBaoCao", controller.dsBaoCao);
 router.get("/dangnhap", controller.dangnhap);
+
+// router.post("/guiBaoCao", controller.guiBaoCao);
+router.post("/guiBaoCao", async (req, res) => {
+    const recaptchaResponse = req.body['g-recaptcha-response'];
+    // Verify reCAPTCHA response
+    try {
+        const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${YOUR_RECAPTCHA_SECRET_KEY}&response=${recaptchaResponse}`;
+        const recaptchaVerification = await axios.post(verificationURL);
+        
+        if (recaptchaVerification.data.success) {
+            await controller.guiBaoCao(req, res);
+        } else {
+            // Handle failed reCAPTCHA verification
+            res.status(403).send('Failed reCAPTCHA verification.');
+        }
+    } catch (error) {
+        console.error('Error verifying reCAPTCHA:', error);
+        res.status(500).send('Internal server error.');
+    }
+});
 
 module.exports = router;
