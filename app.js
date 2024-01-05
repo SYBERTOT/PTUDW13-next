f0bb9f0bd4347f9104bda6d98d9c476c4a72a87b
hoaithu842 <hoaithu842@gmail.com>
2024-01-06 00:34:17 +0700

    update file limit
    hong biet dung hong D:

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ app.engine(
 );
 app.set("view engine","hbs");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
 app.use(cookieParser("COOKIE_SECRET"));
 
