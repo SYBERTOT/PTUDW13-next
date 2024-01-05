a8c0eb75e54d8acce51077f93843301a4aa7a5b3
Le Thanh Trung <letrung148257@gmail.com>
2024-01-05 23:27:53 +0700

    up column hinhthucxuly

--- models/baocao.js
+++ models/baocao.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
     DienThoai: DataTypes.STRING,
     laDiemDat: DataTypes.BOOLEAN,
     XuLy: DataTypes.BOOLEAN,
+    HinhThucXuLy: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'BaoCao',
