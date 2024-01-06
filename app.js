432221be1f60e3777442416c584456101b2c4799
hoaithu842 <hoaithu842@gmail.com>
2024-01-06 16:10:56 +0700

    login with email facebook

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const paginate = require('express-handlebars-paginate');
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const YOUR_APP_ID = "1363610574264678";
+const YOUR_APP_SECRET = "b35089f5d08347d5302206f1a0851782";
+
+const TaiKhoan = require("./models").TaiKhoan;
+const LoaiTaiKhoan = require("./models").LoaiTaiKhoan;
+
 app.use(express.static(__dirname +"/html"));
 app.engine(
     "hbs",
@@ -60,7 +68,58 @@ app.use(
 	  },
 	})
 );
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new FacebookStrategy({
+    clientID: YOUR_APP_ID,
+    clientSecret: YOUR_APP_SECRET,
+    callbackURL: 'http://localhost:4000/dangnhap/facebook/callback',
+	profileFields: ['id', 'emails', 'displayName'],
+	profileFields: ['emails'],
+	// request the user's email
+    scope: ['email'],
+}, async (accessToken, refreshToken, profile, done) => {
+	let Email = profile.emails[0].value;
+	console.log(Email);
+	const taikhoan = await TaiKhoan.findOne({ 
+		attributes: [ "id", "Email", "TenTaiKhoan", "MatKhau", "LoaiTaiKhoanId",],
+		where: { Email },
+		include: [{ model: LoaiTaiKhoan, attributes: ["HoTen"] }]
+	});
 
+	if (taikhoan) {
+
+		return done(null, taikhoan);
+
+	} else {
+		return done(null, null);
+
+	}
+
+}));
+passport.serializeUser((user, done) => {
+    // Serialize user information to store in the session
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    // Deserialize user from the session
+    done(null, obj);
+});
+
+app.get('/dangnhap/facebook', passport.authenticate('facebook'));
+app.get('/dangnhap/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: 'localhost:4000/dangnhap' }),
+    (req, res) => {
+		console.log(req.user);
+		if (req.user) {
+			req.session.taikhoan = req.user;
+			return res.redirect("/");
+		}
+		return res.redirect("/dangnhap");
+
+    }
+);
 app.use("/", require("./routes/authRouter"));
 
 app.get('/sync', function(req,res){
